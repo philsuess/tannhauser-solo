5c972a69cdef8f742a99df11e8e9b98712ef042b
suess <philipp.suess@itwm.fraunhofer.de>
2018-07-03 12:48:42 +0200

    fix layout

--- src/main/App/index.tsx
+++ src/main/App/index.tsx
@@ -5,17 +5,15 @@ import * as SoloApp from "modules/tannhauser";
 import styles from "./styles.css";
 
 const App = () => (
-  <div className={styles.layout}>
-    <main className={styles.content}>
-      <Route
-        exact
-        path="/"
-        component={() => (
-          <SoloApp.App.Component />
-        )}
-      />
-    </main>
-  </div>
+  <main className={styles.content}>
+    <Route
+      exact
+      path="/"
+      component={() => (
+        <SoloApp.App.Component />
+      )}
+    />
+  </main>
 );
 
 export default hot(module)(App);
