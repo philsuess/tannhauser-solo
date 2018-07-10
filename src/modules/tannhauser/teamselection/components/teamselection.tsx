64306e18e17b10f9cb366b8b81053dc71eec7243
suess <philipp.suess@itwm.fraunhofer.de>
2018-07-10 23:30:45 +0200

    improve pack indicator

--- src/modules/tannhauser/teamselection/components/teamselection.tsx
+++ src/modules/tannhauser/teamselection/components/teamselection.tsx
@@ -137,8 +137,12 @@ export default class TeamSelectionMat extends React.Component<TeamSelectionProps
   renderPackIndicator(pack: string) {
     const packColor = Model.GetPackColor(pack);
     const tipString = pack === "" ? "no pack selected" : pack + " pack selected";
+    const circleStrokeWidth = pack === "" ? 0 : 2;
     return <div data-tip={tipString}>
-        <svg width={20} height={20} ><circle cx={10} cy={10} r={10} fill={packColor} /></svg>
+        <svg width={20} height={20} >
+          <circle cx={10} cy={10} r={10} 
+            stroke={"grey"} stroke-width={circleStrokeWidth} fill={packColor} />
+        </svg>
         <ReactTooltip place="left" type="dark" effect="float"/>
       </div>
   }
@@ -157,7 +161,7 @@ export default class TeamSelectionMat extends React.Component<TeamSelectionProps
             </div>
           </div>
           <h3>{char.name}</h3>
-          {this.state[key].selected && this.renderPackIndicator(this.state[key].pack)}
+          {this.renderPackIndicator(this.state[key].pack)}
         </div>
       );
     });
